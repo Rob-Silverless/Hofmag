bafb885b0354318ea57e62d2d7faaab770117429
rc-mei <45292307+rc-mei@users.noreply.github.com>
2020-02-10 18:02:53 +0000

    Standard Page Padding Top fix

--- page-templates/standard-page.php
+++ page-templates/standard-page.php
@@ -6,7 +6,7 @@
  */
 get_header();?>
 
-<div class="container cols-sm-24 align-sm-center grid-gap pt10">
+<div class="container cols-sm-24 align-sm-center grid-gap pt5">
 	<div class="col">
 		<?php the_field('content');?>
 	</div>
